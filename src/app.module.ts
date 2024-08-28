338ffc72f943e954b86533b36b76f110a4e94bf5
Atílio Ferreira <atilio.silvaferreira@gmail.com>
2024-08-28 17:11:47 -0300

    Bonus add docker - Exercicio 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,19 @@ import { TasksModule } from './modules/tasks/tasks.module';
 import { TypeOrmConfig } from './modules/config/typeorm/typeorm.module';
 import { PaginationModule } from './modules/pagination/pagination.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from "cache-manager-redis-store";
 
 @Module({
-  imports: [ProjectsModule, 
-    UsersModule, 
-    TasksModule, 
-    TypeOrmConfig, 
+  imports: [ProjectsModule,
+    UsersModule,
+    TasksModule,
+    TypeOrmConfig,
     PaginationModule,
     CacheModule.register({
-        isGlobal: true
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
   ],
   controllers: [AppController],
